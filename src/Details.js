ef0ebaaa15f0b3f753d741bf4670865bde9b0230
Zack Yang <zack9433@gmail.com>
2019-01-29 09:50:05 +0800

    refactor: modify detail page

--- src/Details.js
+++ src/Details.js
@@ -1,8 +1,50 @@
 import React from 'react';
+import pf from 'petfinder-client';
+
+const petfinder = pf();
 
 class Details extends React.Component {
+  state = {
+    loading: true
+  };
+  componentDidMount() {
+    const { id } = this.props;
+    petfinder.pet
+      .get({
+        output: 'full',
+        id
+      })
+      .then(data => {
+        const breed = Array.isArray(data.petfinder.pet.breeds.breed)
+          ? data.petfinder.pet.breeds.breed.join(', ')
+          : data.petfinder.pet.breeds.breed;
+        this.setState({
+          name: data.petfinder.pet.name,
+          animal: data.petfinder.pet.animal,
+          location: `${data.petfinder.pet.contact.city}, ${
+            data.petfinder.pet.state
+          }`,
+          description: data.petfinder.pet.description,
+          media: data.petfinder.pet.media,
+          breed,
+          loading: false
+        });
+      });
+  }
   render() {
-    return <h1>{this.props.id}</h1>;
+    if (this.state.loading) {
+      return <h1>loading....</h1>;
+    }
+    const { name, animal, location, description, breed } = this.state;
+    return (
+      <div className="details">
+        <div>
+          <h1>{name}</h1>
+          <h2>{`${animal} - ${breed} - ${location}`}</h2>
+          <p>{description}</p>
+        </div>
+      </div>
+    );
   }
 }
 
